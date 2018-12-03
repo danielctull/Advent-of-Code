8b5d58119d1c53b30a3274b9f32d11cfc4145f54
Daniel Tull <dt@danieltull.co.uk>
2018-12-03 17:24:52 +0000

    Use a lazy accumulating sequence and first duplicate function

--- Year2018/Source/Day01.swift
+++ Year2018/Source/Day01.swift
@@ -8,36 +8,24 @@ public struct Day01 {
 
     public func part1(input: Input) -> Int {
 
-        let result = input
+        return input
             .lines
             .compactMap { Int($0.string) }
-            .reduce(into: 0, +=)
-
-        return result
+            .reduce(0, +)
     }
 
     public func part2(input: Input) -> Int {
 
-        var frequencies: Set<Int> = []
-        var frequency = 0
-
-        var lines = input
+        return input
             .lines
-            .repeating
-            .lazy
             .compactMap { Int($0.string) }
-            .makeIterator()
-
-        while !frequencies.contains(frequency) {
-            frequencies.insert(frequency)
-            frequency += lines.next()!
-        }
-
-        return frequency
+            .repeating
+            .accumulating(0, +)
+            .firstDuplicate!
     }
 }
 
-
+// MARK: - RepeatingSequence
 
 extension Sequence {
 
@@ -78,5 +66,74 @@ extension RepeatingSequence.Iterator: IteratorProtocol {
 
         iterator = base.makeIterator()
         return iterator.next()
+    }
+}
+
+// MARK: - AccumulatingSequence
+
+extension Sequence {
+
+    func accumulating<Result>(
+        _ initial: Result,
+        _ transform: @escaping (Result, Element) -> Result
+        ) -> AccumulatingSequence<Self, Result> {
+
+        return AccumulatingSequence(base: self, initial: initial, transform: transform)
+    }
+}
+
+struct AccumulatingSequence<Base: Sequence, Result> {
+    let base: Base
+    let initial: Result
+    let transform: (Result, Base.Element) -> Result
+}
+
+extension AccumulatingSequence: Sequence {
+
+    func makeIterator() -> Iterator {
+        return Iterator(iterator: base.makeIterator(), value: initial, transform: transform)
+    }
+}
+
+extension AccumulatingSequence {
+
+    struct Iterator {
+        var iterator: Base.Iterator
+        var value: Result?
+        var transform: (Result, Base.Element) -> Result
+    }
+}
+
+extension AccumulatingSequence.Iterator: IteratorProtocol {
+
+    mutating func next() -> Result? {
+
+        guard let current = value else {
+            return nil
+        }
+
+        if let element = iterator.next() {
+            value = transform(current, element)
+        } else {
+            value = nil
+        }
+
+        return current
+    }
+}
+
+// MARK: - First Duplicate
+
+extension Sequence where Element: Hashable {
+
+    var firstDuplicate: Element? {
+
+        var elements: Set<Element> = []
+
+        return first { element -> Bool in
+            let contains = elements.contains(element)
+            elements.insert(element)
+            return contains
+        }
     }
 }
