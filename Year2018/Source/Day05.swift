75f42874f1b321c0ce43bc0d6d6e95e0214b94b6
Daniel Tull <dt@danieltull.co.uk>
2018-12-08 17:38:59 +0000

    Add 2018, day 5, part 2

--- Year2018/Source/Day05.swift
+++ Year2018/Source/Day05.swift
@@ -8,50 +8,90 @@ public struct Day05 {
 
     public func part1(input: Input) -> Int {
 
-        guard var result = input.lines.first?.string else {
-            return 0
-        }
-
-        var previousResult = ""
-
-        while result.count != previousResult.count {
-
-            previousResult = result
-
-            var previousCharacter: Character?
-
-            result = previousResult.reduce(into: "") { result, character in
-
-                guard let previous = previousCharacter else {
-                    previousCharacter = character
-                    return
-                }
-
-                if previous.isEqualButOppositeCase(to: character) {
-                    previousCharacter = nil
-                } else {
-                    result.append(previous)
-                    previousCharacter = character
-                }
-            }
-
-            if let previousCharacter = previousCharacter {
-                result.append(previousCharacter)
-            }
-        }
-
-        return result.count
+        return input
+            .lines
+            .first?
+            .string
+            .map {$0}
+            .reducingPolymers
+            .count ?? 0
     }
 
     public func part2(input: Input) -> Int {
-        return 100
+
+        guard let characters = input
+            .lines
+            .first?
+            .string
+            .map({$0})
+        else {
+            return 0
+        }
+
+        let uniqueCharacters = Set(characters.map { $0.lowercased })
+
+        return uniqueCharacters.map { character in
+
+            return characters
+                .filter { $0.lowercased != character }
+                .reducingPolymers
+                .count
+
+        }.min() ?? 0
     }
 }
 
 extension Character {
 
-    func isEqualButOppositeCase(to character: Character) -> Bool {
+    var lowercased: Character {
+        return Character(String(self).lowercased())
+    }
+}
 
-        return character != self && String(character).uppercased() == String(self).uppercased()
+extension Array where Element == Character {
+
+    var reducingPolymers: [Character] {
+
+        var result = self
+        var previousResult: [Character] = []
+
+        while result.count != previousResult.count {
+            previousResult = result
+            result = previousResult.removingPairs(shouldRemove: { lhs, rhs -> Bool in
+                return lhs != rhs && lhs.lowercased == rhs.lowercased
+            })
+        }
+
+        return result
+    }
+}
+
+
+extension Sequence {
+
+    func removingPairs(shouldRemove: (Element, Element) -> Bool) -> [Element] {
+
+        var previousValue: Element?
+        
+        var result: [Element] = reduce(into: []) { result, value in
+
+            guard let previous = previousValue else {
+                previousValue = value
+                return
+            }
+
+            if shouldRemove(previous, value) {
+                previousValue = nil
+            } else {
+                result.append(previous)
+                previousValue = value
+            }
+        }
+
+        if let previousValue = previousValue {
+            result.append(previousValue)
+        }
+
+        return result
     }
 }
--- Year2018/Tests/Day05Tests.swift
+++ Year2018/Tests/Day05Tests.swift
@@ -16,14 +16,14 @@ final class Day05Tests: XCTestCase {
         XCTAssertEqual(result, 10888)
     }
 
-//    func testPart2Example() {
-//        let result = Day05().part1(input: "")
-//        XCTAssertEqual(result, 0)
-//    }
-//
-//    func testPart2Puzzle() throws {
-//        let file = try Input(named: "Day05")
-//        let result = Day05().part2(input: file)
-//        XCTAssertEqual(result, 3852)
-//    }
+    func testPart2Example() {
+        let result = Day05().part2(input: "dabAcCaCBAcCcaDA")
+        XCTAssertEqual(result, 4)
+    }
+
+    func testPart2Puzzle() throws {
+        let file = try Input(named: "Day05")
+        let result = Day05().part2(input: file)
+        XCTAssertEqual(result, 6952)
+    }
 }
