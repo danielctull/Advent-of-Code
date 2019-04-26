1ae48b8854b68e43b946fe88e68269c42dec7db6
Daniel Tull <dt@danieltull.co.uk>
2019-04-26 16:58:05 -0400

    Add LinuxMain

---
+++ Year2015/LinuxMain.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+import Year2015_Tests
+
+var tests = [XCTestCaseEntry]()
+tests += Year2015_Tests.__allTests()
+
+XCTMain(tests)
---
+++ Year2018/LinuxMain.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+import Year2018_Tests
+
+var tests = [XCTestCaseEntry]()
+tests += Year2018_Tests.__allTests()
+
+XCTMain(tests)
