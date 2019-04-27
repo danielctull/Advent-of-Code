9ba84cd87b324118c2f35bc495ea4038c42fbe83
Daniel Tull <dt@danieltull.co.uk>
2019-04-26 21:59:34 -0400

    Try to fix the filtering

--- LinuxMain.swift
+++ LinuxMain.swift
@@ -8,6 +8,9 @@ tests += Year2015_Tests.__allTests()
 tests += Year2018_Tests.__allTests()
 
 // Filter out all the puzzles because they load a file from disk
-tests = tests.filter { !$0.0.contains("Puzzle") }
+tests = tests.map {
+    let tests = $0.allTests.filter { !$0.contains("Puzzle") }
+    return ($0.testCaseClass, tests)
+}
 
 XCTMain(tests)
