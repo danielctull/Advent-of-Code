386ac1dd7be9a2162f87d21559a4d5a2f4ec9f74
Daniel Tull <dt@danieltull.co.uk>
2019-12-02 10:58:25 +0000

    Add 2019, day 1, part 1

--- Year2019/Source/Day01.swift
+++ Year2019/Source/Day01.swift
@@ -1,1 +1,17 @@
 
+import Advent
+import Foundation
+
+public struct Day01 {
+
+    public init() {}
+
+    public func part1(input: Input) -> Int {
+
+        input
+            .lines
+            .compactMap { Double($0.string) }
+            .map { Int(floor($0 / 3)) - 2 }
+            .reduce(0, +)
+    }
+}
--- Year2019/Tests/Day01Tests.swift
+++ Year2019/Tests/Day01Tests.swift
@@ -4,4 +4,31 @@ import Year2019
 import XCTest
 
 final class Day01Tests: XCTestCase {
+
+    func testPart1Example1() {
+        let result = Day01().part1(input: "12")
+        XCTAssertEqual(result, 2)
+    }
+
+    func testPart1Example2() {
+        let result = Day01().part1(input: "14")
+        XCTAssertEqual(result, 2)
+    }
+
+    func testPart1Example3() {
+        let result = Day01().part1(input: "1969")
+        XCTAssertEqual(result, 654)
+    }
+
+    func testPart1Example4() {
+        let result = Day01().part1(input: "100756")
+        XCTAssertEqual(result, 33583)
+    }
+
+    func testPart1Puzzle() throws {
+        let day = Day01()
+        let file = try Input(named: "Day01")
+        let result = day.part1(input: file)
+        XCTAssertEqual(result, 3488702)
+    }
 }
