77fb565c1e61c06ec0579e3da81f0bfdb3c1111c
Daniel Tull <dt@danieltull.co.uk>
2019-12-05 07:31:28 +0000

    Add Parameter type

--- Year2019/Source/IntcodeComputer.swift
+++ Year2019/Source/IntcodeComputer.swift
@@ -12,10 +12,10 @@ public struct IntcodeComputer {
         while instructionPointer < memory.count {
 
             func perform(_ closure: (Int, Int) -> Int) {
-                let position1 = memory[instructionPointer + 1]
-                let position2 = memory[instructionPointer + 2]
-                let position3 = memory[instructionPointer + 3]
-                memory[position3] = closure(memory[position1], memory[position2])
+                let parameter1 = Parameter.position(instructionPointer + 1)
+                let parameter2 = Parameter.position(instructionPointer + 2)
+                let parameter3 = Parameter.position(instructionPointer + 3)
+                self[parameter3] = closure(self[parameter1], self[parameter2])
                 instructionPointer += 4
             }
 
@@ -28,5 +28,28 @@ public struct IntcodeComputer {
         }
 
         fatalError()
+    }
+}
+
+extension IntcodeComputer {
+
+    enum Parameter {
+        case immediate(Int)
+        case position(Int)
+    }
+
+    subscript(parameter: Parameter) -> Int {
+        get {
+            switch parameter {
+            case let .immediate(pointer): return memory[pointer]
+            case let .position(pointer): return memory[memory[pointer]]
+            }
+        }
+        set(newValue) {
+            switch parameter {
+            case let .immediate(pointer): memory[pointer] = newValue
+            case let .position(pointer): memory[memory[pointer]] = newValue
+            }
+        }
     }
 }
