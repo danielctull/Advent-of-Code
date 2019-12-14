9fb43b33926654dd27730c30584fd2646cd534e2
Daniel Tull <dt@danieltull.co.uk>
2019-12-14 10:31:07 +0000

    Add 2019, day 14, part 2
    Second dumb implementation of the day! This doesn't complete in any decent time, but did complete part2, example 1 in just under a minute.

--- Year2019/Source/Day14.swift
+++ Year2019/Source/Day14.swift
@@ -7,16 +7,45 @@ public struct Day14 {
     public init() {}
 
     public func part1(input: Input) throws -> Int {
+        let processor = FuelProcessor(input: input)
+        var excess: [String] = []
+        var requiredOre = 0
+        processor.createFuel(excess: &excess, ore: &requiredOre)
+        return requiredOre
+    }
 
-        let reactions: [String: Reaction] = input
-            .lines
-            .map { Reaction($0.string) }
-            .group(by: { $0.output[0] })
-            .mapValues { $0[0] }
+    public func part2(input: Input) throws -> Int {
+        let processor = FuelProcessor(input: input)
+        let storedOre = 1000000000000
+        var excess: [String] = []
+        var requiredOre = 0
+        var fuel = 0
+
+        repeat {
+            processor.createFuel(excess: &excess, ore: &requiredOre)
+            fuel += 1
+        } while requiredOre < storedOre && excess.count != 0
+
+        let amount = storedOre / requiredOre
+        fuel *= amount
+        requiredOre *= amount
+
+        repeat {
+            processor.createFuel(excess: &excess, ore: &requiredOre)
+            fuel += 1
+        } while requiredOre < storedOre
+
+        return fuel - 1 // Take into account the over production in the final loop.
+    }
+}
+
+fileprivate struct FuelProcessor {
+
+    let reactions: [String: Reaction]
+
+    func createFuel(excess: inout [String], ore: inout Int) {
 
         var required = reactions["FUEL"]!.input
-        var excess: [String] = []
-        var ore = 0
 
         while let chemical = required.first {
 
@@ -30,8 +59,14 @@ public struct Day14 {
                 ore += reaction.ore
             }
         }
+    }
 
-        return ore
+    init(input: Input) {
+        reactions = input
+            .lines
+            .map { Reaction($0.string) }
+            .group(by: { $0.output[0] })
+            .mapValues { $0[0] }
     }
 }
 
