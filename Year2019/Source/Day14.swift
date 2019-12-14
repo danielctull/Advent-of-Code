59340bdee525a60aa10f785eb569152a35af6e98
Daniel Tull <dt@danieltull.co.uk>
2019-12-14 12:15:43 +0000

    Increasing speed for 2019, day 14
    Still too slow for part 2 though.

--- Year2019/Source/Day14.swift
+++ Year2019/Source/Day14.swift
@@ -8,7 +8,7 @@ public struct Day14 {
 
     public func part1(input: Input) throws -> Int {
         let processor = FuelProcessor(input: input)
-        var excess: [String] = []
+        var excess: [Chemical: Int] = [:]
         var requiredOre = 0
         processor.createFuel(excess: &excess, ore: &requiredOre)
         return requiredOre
@@ -17,14 +17,14 @@ public struct Day14 {
     public func part2(input: Input) throws -> Int {
         let processor = FuelProcessor(input: input)
         let storedOre = 1000000000000
-        var excess: [String] = []
+        var excess: [Chemical: Int] = [:]
         var requiredOre = 0
         var fuel = 0
 
         repeat {
             processor.createFuel(excess: &excess, ore: &requiredOre)
             fuel += 1
-        } while requiredOre < storedOre && excess.count != 0
+        } while requiredOre < storedOre && excess.filter({ $0.value > 0 }).count > 0
 
         let amount = storedOre / requiredOre
         fuel *= amount
@@ -41,59 +41,66 @@ public struct Day14 {
 
 fileprivate struct FuelProcessor {
 
-    let reactions: [String: Reaction]
+    let reactions: [Chemical: Reaction]
 
-    func createFuel(excess: inout [String], ore: inout Int) {
+    func createFuel(excess: inout [Chemical: Int], ore: inout Int) {
 
-        var required = reactions["FUEL"]!.input
+        var required = reactions[.fuel]!.input
 
-        while let chemical = required.first {
+        while let (chemical, amount) = required.first(where: { $0.value > 0 && $0.key != .ore }) {
 
-            if let excessIndex = excess.firstIndex(of: chemical) {
-                required.removeFirst()
-                excess.remove(at: excessIndex)
+            if let excessChemical = excess[chemical], excessChemical > 0 {
+                let remainingRequired = max(amount - excessChemical, 0)
+                let remainingExcess = max(excessChemical - amount, 0)
+                required[chemical] = remainingRequired
+                excess[chemical] = remainingExcess
             } else {
                 let reaction = reactions[chemical]!
-                required.append(contentsOf: reaction.input)
-                excess.append(contentsOf: reaction.output)
-                ore += reaction.ore
+                required.merge(reaction.input, uniquingKeysWith: +)
+                excess[chemical, default: 0] += reaction.output.1
             }
         }
+
+        ore += required[.ore]!
     }
 
     init(input: Input) {
         reactions = input
             .lines
             .map { Reaction($0.string) }
-            .group(by: { $0.output[0] })
+            .group(by: { $0.output.0 })
             .mapValues { $0[0] }
     }
 }
 
+fileprivate struct Chemical: Equatable, Hashable {
+    static let ore = Chemical(value: "ORE")
+    static let fuel = Chemical(value: "FUEL")
+    let value: String
+}
+
 fileprivate struct Reaction {
-    let input: [String]
-    let output: [String]
-    let ore: Int
+    let input: [Chemical: Int]
+    let output: (Chemical, Int)
 
     init(_ string: String) {
 
-        func chemicals(from string: String) -> [String] {
+        func chemicals(from string: String) -> (Chemical, Int) {
             let values = string
                 .trimmingCharacters(in: .whitespaces)
                 .components(separatedBy: " ")
+            let chemical = Chemical(value: values[1])
             let amount = Int(values[0])!
-            let ingredient = values[1]
-            return Array(repeating: ingredient, count: amount)
+            return (chemical, amount)
         }
 
         let recipe = string.components(separatedBy: "=>")
         output = chemicals(from: recipe[1])
 
-        let inputChemicals = recipe[0]
+        input = recipe[0]
             .components(separatedBy: ",")
-            .flatMap(chemicals(from:))
-
-        input = inputChemicals.filter { $0 != "ORE" }
-        ore = inputChemicals.count - input.count
+            .map(chemicals(from:))
+            .group(by: { $0.0 })
+            .mapValues { $0[0].1 }
     }
 }
