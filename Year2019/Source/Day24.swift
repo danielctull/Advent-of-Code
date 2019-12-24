d807c5e6b75d5d67edca8f6dd8c6ffb13004b677
Daniel Tull <dt@danieltull.co.uk>
2019-12-24 17:18:48 +0000

    Add 2019, day 24, part 2

--- Year2019/Source/Day24.swift
+++ Year2019/Source/Day24.swift
@@ -8,11 +8,13 @@ public struct Day24 {
 
     public func part1(input: Input) throws -> Int {
 
-        var states = Set<Grid<Position, Day24.Tile>>()
-        var current = try Grid<Position, Day24.Tile>(input: input)
+        var states = Set<Grid<Position, Tile>>()
+        var current = try Grid<Position, Tile>(input: input)
 
         while true {
-            let new = current.nextState()
+            let new = current.nextState { position in
+                position.adjacent.filter { current[$0] == .bug }.count
+            }
 
             guard !states.contains(new) else {
                 return new.biodiversityRating
@@ -21,6 +23,43 @@ public struct Day24 {
             states.insert(new)
             current = new
         }
+    }
+
+    public func part2(input: Input, count: Int = 200) throws -> Int {
+
+        var level0 = try Grid<Position, Day24.Tile>(input: input)
+        level0.tiles.removeValue(forKey: Position(x: 2, y: 2))
+
+        var empty = Grid(width: 5, height: 5) { _ in Day24.Tile.empty }
+        empty.tiles.removeValue(forKey: Position(x: 2, y: 2))
+
+        let tiles = (-count...count)
+            .flatMap { level -> [(Location, Tile)] in
+
+                switch level {
+                case 0:
+                    return level0.tiles.map { (position: Position, tile: Day24.Tile) in
+                        (Location(level: level, position: position), tile)
+                    }
+                default:
+                    return empty.tiles.map { (position: Position, tile: Day24.Tile) in
+                        (Location(level: level, position: position), .empty)
+                    }
+                }
+            }
+            .group(by: { $0.0 })
+            .mapValues { $0[0].1 }
+
+        let grid = (1...count).reduce(Grid(tiles: tiles)) { grid, phase in
+            grid.nextState { location in
+                location.adjacent.filter { grid[$0] == .bug }.count
+            }
+        }
+
+        return grid
+            .tiles
+            .filter { $0.value == .bug }
+            .count
     }
 }
 
@@ -32,18 +71,79 @@ extension Grid where Location == Position, Tile == Day24.Tile {
             .map { 2.power($0.key.y * 5 + $0.key.x) }
             .reduce(0, +)
     }
+}
 
-    func nextState() -> Self {
+extension Grid where Tile == Day24.Tile {
 
-        mapTiles { position, tile in
+    func nextState(adjacentBugCount: (Location) -> Int) -> Self {
 
-            let adjacentBugs = position.adjacent.filter { self[$0] == .bug }
-
-            switch (tile, adjacentBugs.count) {
+        mapTiles { location, tile in
+            switch (tile, adjacentBugCount(location)) {
             case (.bug, 1): return .bug
             case (.bug, _): return .empty
             case (.empty, 1...2): return .bug
             case (.empty, _): return .empty
+            }
+        }
+    }
+}
+
+// MARK: - Location
+
+extension Day24 {
+
+    public struct Location {
+        public let level: Int
+        public let position: Position
+
+        public init(level: Int, position: Position) {
+            self.level = level
+            self.position = position
+        }
+    }
+}
+
+extension Day24.Location: Equatable {}
+extension Day24.Location: Hashable {}
+
+extension Day24.Location {
+
+    public var adjacent: [Day24.Location] {
+
+        position.adjacent.flatMap { adjacent -> [Day24.Location] in
+
+            switch (position.x, position.y, adjacent.x, adjacent.y) {
+
+            // Adjacent is middle
+            case (2,1, 2,2):
+                return (0...4).map { Self(level: level + 1, position: Position(x: $0, y: 0)) }
+
+            case (2,3, 2,2):
+                return (0...4).map { Self(level: level + 1, position: Position(x: $0, y: 4)) }
+
+            case (1,2, 2,2):
+                return (0...4).map { Self(level: level + 1, position: Position(x: 0, y: $0)) }
+
+            case (3,2, 2,2):
+                return (0...4).map { Self(level: level + 1, position: Position(x: 4, y: $0)) }
+
+            // Adjacent is outside
+            case (_,_, -1,_):
+                return [ Self(level: level - 1, position: Position(x: 1, y: 2)) ]
+
+            case (_,_, 5,_):
+                return [ Self(level: level - 1, position: Position(x: 3, y: 2)) ]
+
+            case (_,_, _,-1):
+                return [ Self(level: level - 1, position: Position(x: 2, y: 1)) ]
+
+            case (_,_, _,5):
+                return [ Self(level: level - 1, position: Position(x: 2, y: 3)) ]
+
+            // Adjacent is in this level
+
+            default:
+                return [ Self(level: level, position: adjacent) ]
             }
         }
     }
--- Year2019/Tests/Day24Tests.swift
+++ Year2019/Tests/Day24Tests.swift
@@ -22,4 +22,84 @@ final class Day24Tests: XCTestCase {
         let result = try day.part1(input: file)
         XCTAssertEqual(result, 18842609)
     }
+
+    func testLocationAdjacency1() {
+        let adjacent = Day24.Location(level: 0, position: Position(x: 3, y: 3)).adjacent
+        XCTAssertEqual(adjacent.count, 4)
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 2, y: 3))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 4, y: 3))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 3, y: 2))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 3, y: 4))))
+    }
+
+    func testLocationAdjacency2() {
+        let adjacent = Day24.Location(level: 1, position: Position(x: 1, y: 1)).adjacent
+        XCTAssertEqual(adjacent.count, 4)
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 1, y: 0))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 0, y: 1))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 2, y: 1))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 1, y: 2))))
+    }
+
+    func testLocationAdjacency3() {
+        let adjacent = Day24.Location(level: 1, position: Position(x: 3, y: 0)).adjacent
+        XCTAssertEqual(adjacent.count, 4)
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 2, y: 1))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 2, y: 0))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 4, y: 0))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 3, y: 1))))
+    }
+
+    func testLocationAdjacency4() {
+        let adjacent = Day24.Location(level: 1, position: Position(x: 4, y: 0)).adjacent
+        XCTAssertEqual(adjacent.count, 4)
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 2, y: 1))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 3, y: 0))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 3, y: 2))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 4, y: 1))))
+    }
+
+    func testLocationAdjacency5() {
+        let adjacent = Day24.Location(level: 0, position: Position(x: 3, y: 2)).adjacent
+        XCTAssertEqual(adjacent.count, 8)
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 3, y: 1))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 4, y: 0))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 4, y: 1))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 4, y: 2))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 4, y: 3))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 4, y: 4))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 4, y: 2))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 0, position: Position(x: 3, y: 3))))
+    }
+
+    func testLocationAdjacency6() {
+        let adjacent = Day24.Location(level: 1, position: Position(x: 3, y: 2)).adjacent
+        XCTAssertEqual(adjacent.count, 8)
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 3, y: 1))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 4, y: 2))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 1, position: Position(x: 3, y: 3))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 2, position: Position(x: 4, y: 0))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 2, position: Position(x: 4, y: 1))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 2, position: Position(x: 4, y: 2))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 2, position: Position(x: 4, y: 3))))
+        XCTAssert(adjacent.contains(Day24.Location(level: 2, position: Position(x: 4, y: 4))))
+    }
+
+    func testPart2Example1() throws {
+        let result = try Day24().part2(input: [
+            "....#",
+            "#..#.",
+            "#..##",
+            "..#..",
+            "#...."
+        ], count: 10)
+        XCTAssertEqual(result, 99)
+    }
+
+    func testPart2Puzzle() throws {
+        let day = Day24()
+        let file = try Input(named: "Day24")
+        let result = try day.part2(input: file)
+        XCTAssertEqual(result, 2059)
+    }
 }
