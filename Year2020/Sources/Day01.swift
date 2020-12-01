181c5ec50e735333ae30099911d83909a07fa044
Daniel Tull <dt@danieltull.co.uk>
2020-12-01 21:53:08 +0000

    Improve speed of 2020, day 1
    Rough time improvements
      Part 1:   20ms ->  1ms
      Part 2: 1300ms -> 13ms

--- Year2020/Sources/Day01.swift
+++ Year2020/Sources/Day01.swift
@@ -6,18 +6,17 @@ import Foundation
 public enum Day01 {
 
     public static func part1(_ input: Input) -> Int {
-
-        input.integers
-            .combinations(ofCount: 2)
-            .first(where: { $0.sum() == 2020 })
-            .map { $0.product() } ?? 0
+        let values = Set(input.integers)
+        return input.integers
+            .first(where: { values.contains(2020 - $0) })
+            .map { $0 * (2020 - $0) } ?? 0
     }
 
     public static func part2(_ input: Input) -> Int {
-
-        input.integers
-            .combinations(ofCount: 3)
-            .first(where: { $0.sum() == 2020 })
-            .map { $0.product() } ?? 0
+        let values = Set(input.integers)
+        return input.integers
+            .combinations(ofCount: 2)
+            .first(where: { values.contains(2020 - $0.sum()) })
+            .map { $0.product() * (2020 - $0.sum()) } ?? 0
     }
 }
