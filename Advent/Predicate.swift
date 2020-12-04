59a7527d69fc208d4d7a975cda6755e8d7a7472e
Daniel Tull <dt@danieltull.co.uk>
2020-12-04 14:41:52 +0000

    Rename Pattern to Predicate

---
+++ Advent/Predicate.swift
@@ -0,0 +1,23 @@
+
+public struct Predicate<Value> {
+
+    private let predicate: (Value) -> Bool
+    public init(_ predicate: @escaping (Value) -> Bool) {
+        self.predicate = predicate
+    }
+
+    public func callAsFunction(_ value: Value) -> Bool {
+        predicate(value)
+    }
+}
+
+public func ~= <T>(predicate: Predicate<T>, value: T) -> Bool {
+    predicate(value)
+}
+
+extension Predicate where Value == String {
+
+    public static func hasPrefix(_ prefix: String) -> Predicate {
+        Predicate { $0.hasPrefix(prefix) }
+    }
+}
