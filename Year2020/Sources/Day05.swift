eb1319cbc84ca756cfa249303d852ab7dd2fd0bb
Daniel Tull <dt@danieltull.co.uk>
2020-12-05 09:55:32 +0000

    Remove Seat type and use a custom initialiser on BinaryNumber

--- Year2020/Sources/Day05.swift
+++ Year2020/Sources/Day05.swift
@@ -8,33 +8,32 @@ public enum Day05 {
     public static func part1(_ input: Input) throws -> Int {
 
         input.lines
-            .map { Seat($0).id }
+            .map { Int(BinaryNumber(seat: $0)) }
             .max() ?? 0
     }
 
     public static func part2(_ input: Input) -> Int {
 
         input.lines
-            .map { Seat($0).id }
+            .map { Int(BinaryNumber(seat: $0)) }
             .sorted()
             .slidingWindows(ofCount: 2)
             .first(where: { $0.last! - $0.first! == 2 }) //Find gap in seats.
             .map { $0.first! + 1 } ?? 0
     }
+}
 
-    private struct Seat {
-        var id: Int { Int(number) }
-        private let number: BinaryNumber
-        init(_ string: String) {
-            number = BinaryNumber(bits: string.map {
-                switch $0 {
-                case "F": return .zero
-                case "B": return .one
-                case "L": return .zero
-                case "R": return .one
-                default: fatalError()
-                }
-            })
-        }
+extension BinaryNumber {
+
+    fileprivate init(seat: String) {
+        self.init(bits: seat.map {
+            switch $0 {
+            case "F": return .zero
+            case "B": return .one
+            case "L": return .zero
+            case "R": return .one
+            default: fatalError()
+            }
+        })
     }
 }
