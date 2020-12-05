7a986bebd8269e31f61a8379bb330a34f9d6832f
Daniel Tull <dt@danieltull.co.uk>
2020-12-05 07:40:45 +0000

    Reduce the bits using binary number logic

--- Year2020/Sources/Day05.swift
+++ Year2020/Sources/Day05.swift
@@ -22,42 +22,45 @@ public enum Day05 {
             .map { $0.first! + 1 } ?? 0
     }
 
-    private enum Split {
-        case lower
-        case upper
+    private enum Bit {
+        case zero
+        case one
+
+        var value: Int {
+            switch self {
+            case .one: return 1
+            case .zero: return 0
+            }
+        }
     }
 
     private struct Seat {
-        let row: [Split]
-        let column: [Split]
+        let row: [Bit]
+        let column: [Bit]
         init(_ string: String) {
             let array = Array(string)
             row = array[0...6].map {
                 switch $0 {
-                case "F": return .lower
-                case "B": return .upper
+                case "F": return .zero
+                case "B": return .one
                 default: fatalError()
                 }
             }
             column = array[7...9].map {
                 switch $0 {
-                case "L": return .lower
-                case "R": return .upper
+                case "L": return .zero
+                case "R": return .one
                 default: fatalError()
                 }
             }
         }
 
         var number: Int {
-            func split(range: ClosedRange<Int>, split: Split) -> ClosedRange<Int> {
-                let half = range.count / 2
-                switch split {
-                case .lower: return (range.lowerBound...range.upperBound-half)
-                case .upper: return (range.lowerBound+half...range.upperBound)
-                }
+            func calculate(previous: Int, bit: Bit) -> Int {
+                previous * 2 + bit.value
             }
-            let r = row.reduce((0...127), split).lowerBound
-            let c = column.reduce((0...7), split).lowerBound
+            let r = row.reduce(0, calculate)
+            let c = column.reduce(0, calculate)
             return r * 8 + c
         }
     }
