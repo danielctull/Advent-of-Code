e981fba88e729e5faf9aff7919eb19da0aef94c9
Daniel Tull <dt@danieltull.co.uk>
2020-12-08 05:24:49 +0000

    Add 2020, day 8, part 1

---
+++ Year2020/Sources/Day08.swift
@@ -0,0 +1,75 @@
+
+import Advent
+import Foundation
+
+public enum Day08 {
+
+    public static func part1(_ input: Input) throws -> Int {
+
+        var accumulator = 0
+        let instructions = try input.lines
+            .map(Instruction.init)
+
+        instructions.execute(value: &accumulator)
+        return accumulator
+    }
+
+    public static func part2(_ input: Input) throws -> Int {
+        0
+    }
+}
+
+extension Array where Element == Day08.Instruction {
+
+    func execute(value: inout Int) {
+        var visited = Set<Index>()
+        var index = 0
+
+        while !visited.contains(index) {
+            visited.insert(index)
+            let instruction = self[index]
+            switch instruction.kind {
+            case .nop: index += 1
+            case .acc: index += 1; value += instruction.value
+            case .jmp: index += instruction.value
+            }
+        }
+    }
+}
+
+extension Day08 {
+
+    fileprivate struct Instruction {
+        let kind: Kind
+        let value: Int
+    }
+}
+
+extension Day08.Instruction {
+
+    enum Kind {
+        case jmp
+        case nop
+        case acc
+    }
+}
+
+extension Day08.Instruction {
+
+    init(string: String) throws {
+        struct InstructionKindError: Error {}
+        let regex = try RegularExpression(pattern: #"([a-z]{3}) ([+\-0-9]+)"#)
+        let match = try regex.match(string)
+        value = try match.integer(at: 1)
+        switch try match.string(at: 0) {
+        case "jmp": kind = .jmp
+        case "nop": kind = .nop
+        case "acc": kind = .acc
+        default: throw InstructionKindError()
+        }
+    }
+}
+
+extension Day08.Instruction: CustomStringConvertible {
+    var description: String { "Instruction(\(kind), \(value))" }
+}
---
+++ Year2020/Tests/Day08Tests.swift
@@ -0,0 +1,36 @@
+
+import Advent
+import Year2020
+import XCTest
+
+final class Day08Tests: XCTestCase {
+
+    func testPart1Examples() throws {
+        XCTAssertEqual(try Day08.part1([
+            "nop +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "jmp -4",
+            "acc +6"
+        ]), 5)
+    }
+
+    func testPart1Puzzle() throws {
+        let input = try Bundle.module.input(named: "Day08")
+        XCTAssertEqual(try Day08.part1(input), 1501)
+    }
+
+    func testPart2Examples() throws {
+        XCTAssertEqual(try Day08.part2([]), 0)
+        XCTAssertEqual(try Day08.part2([]), 0)
+    }
+
+    func testPart2Puzzle() throws {
+        let input = try Bundle.module.input(named: "Day08")
+        XCTAssertEqual(try Day08.part2(input), 0)
+    }
+}
