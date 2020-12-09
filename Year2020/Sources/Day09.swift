3a5dc3b11211d295682521547b6da42156d3b6a1
Daniel Tull <dt@danieltull.co.uk>
2020-12-09 06:59:17 +0000

    Add 2020, day 9, part 2

--- Year2020/Sources/Day09.swift
+++ Year2020/Sources/Day09.swift
@@ -5,11 +5,29 @@ import Foundation
 public enum Day09 {
 
     public static func part1(_ input: Input) throws -> Int {
+        input.integers.invalidNumber(preamble: input.testing ? 5 : 25)
+    }
 
-        let preamble = input.testing ? 5 : 25
+    public static func part2(_ input: Input) throws -> Int {
 
-        return input.integers
-            .slidingWindows(ofCount: preamble + 1)
+        let numbers = input.integers
+        let invalid = numbers.invalidNumber(preamble: input.testing ? 5 : 25)
+
+        return (2...).lazy
+            .compactMap { count in
+                numbers.slidingWindows(ofCount: count)
+                    .first(where: { $0.sum() == invalid })
+            }
+            .map { $0.min()! + $0.max()! }
+            .first(where: { _ in true }) ?? 0
+    }
+}
+
+extension Array where Element == Int {
+
+    fileprivate func invalidNumber(preamble: Int) -> Int {
+
+        slidingWindows(ofCount: preamble + 1)
             .compactMap { values in
 
                 let value = values.last!
@@ -27,9 +45,5 @@ public enum Day09 {
                 }
             }
             .first ?? 0
-    }
-
-    public static func part2(_ input: Input) throws -> Int {
-        0
     }
 }
--- Year2020/Tests/Day09Tests.swift
+++ Year2020/Tests/Day09Tests.swift
@@ -36,11 +36,31 @@ final class Day09Tests: XCTestCase {
     }
 
     func testPart2Examples() throws {
-        XCTAssertEqual(try Day09.part2([]), 0)
+        XCTAssertEqual(try Day09.part2([
+            "35",
+            "20",
+            "15",
+            "25",
+            "47",
+            "40",
+            "62",
+            "55",
+            "65",
+            "95",
+            "102",
+            "117",
+            "150",
+            "182",
+            "127",
+            "219",
+            "299",
+            "277",
+            "309",
+            "576"]), 62)
     }
 
     func testPart2Puzzle() throws {
         let input = try Bundle.module.input(named: "Day09")
-        XCTAssertEqual(try Day09.part2(input), 0)
+        XCTAssertEqual(try Day09.part2(input), 4389369)
     }
 }
