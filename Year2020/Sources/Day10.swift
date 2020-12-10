4daa3f56a857b1880fdd3dd761aa410960efe617
Daniel Tull <dt@danieltull.co.uk>
2020-12-10 18:02:53 +0000

    Rename variables

--- Year2020/Sources/Day10.swift
+++ Year2020/Sources/Day10.swift
@@ -18,25 +18,25 @@ public enum Day10 {
 
     public static func part2(_ input: Input) throws -> Int {
 
-        let joltages = ([0] + input.integers).sorted()
+        let adapters = ([0] + input.integers).sorted()
 
         func canReach(_ lower: Int, _ upper: Int) -> Bool {
             (0...3).contains(upper - lower)
         }
 
-        // Seed the first three, then we can slide along to sum the different
-        // paths to getting to the end.
-        var reaches = Array(repeating: 1, count: joltages.count)
-        reaches[2] = canReach(joltages[0], joltages[2]) ? 2 : 1
+        // Seed the first three, then we can move along to sum the different
+        // pathways to getting to the end adapter.
+        var pathways = Array(repeating: 1, count: adapters.count)
+        pathways[2] = canReach(adapters[0], adapters[2]) ? 2 : 1
 
-        for index in reaches.indices.dropFirst(3) {
-            let joltage = joltages[index]
-            var reach = 0
-            if canReach(joltages[index - 1], joltage) { reach += reaches[index - 1] }
-            if canReach(joltages[index - 2], joltage) { reach += reaches[index - 2] }
-            if canReach(joltages[index - 3], joltage) { reach += reaches[index - 3] }
-            reaches[index] = reach
+        for index in pathways.indices.dropFirst(3) {
+            let adapter = adapters[index]
+            var pathway = 0
+            if canReach(adapters[index - 1], adapter) { pathway += pathways[index - 1] }
+            if canReach(adapters[index - 2], adapter) { pathway += pathways[index - 2] }
+            if canReach(adapters[index - 3], adapter) { pathway += pathways[index - 3] }
+            pathways[index] = pathway
         }
-        return reaches.last!
+        return pathways.last!
     }
 }
