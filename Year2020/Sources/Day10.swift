c6e4e1bfff40d8aab94643b9d4aa4df3d2ebce56
Daniel Tull <dt@danieltull.co.uk>
2020-12-10 11:32:34 +0000

    Increase speed to 56.7 microseconds for 2020 day 10, part 2

--- Year2020/Sources/Day10.swift
+++ Year2020/Sources/Day10.swift
@@ -18,12 +18,7 @@ public enum Day10 {
 
     public static func part2(_ input: Input) throws -> Int {
 
-        let joltages = ([0] + input.integers)
-            .sorted()
-        var reaches = Array(repeating: 0, count: joltages.count)
-
-        let windows = reaches.indices
-            .slidingWindows(ofCount: 4)
+        let joltages = ([0] + input.integers).sorted()
 
         func canReach(_ lower: Int, _ upper: Int) -> Bool {
             (0...3).contains(upper - lower)
@@ -31,30 +26,16 @@ public enum Day10 {
 
         // Seed the first three, then we can slide along to sum the different
         // paths to getting to the end.
-        
-        reaches[0] = 1
-        reaches[1] = 1
-        reaches[2] = [canReach(joltages[0], joltages[2]), canReach(joltages[1], joltages[2])]
-            .map { $0 ? 1 : 0 }
-            .sum()
+        var reaches = Array(repeating: 1, count: joltages.count)
+        reaches[2] = canReach(joltages[0], joltages[2]) ? 2 : 1
 
-        for indices in windows {
-
-            let index0 = indices.startIndex
-            let index1 = indices.index(after: index0)
-            let index2 = indices.index(after: index1)
-            let index3 = indices.index(after: index2)
-
-            let joltage0 = joltages[index0]
-            let joltage1 = joltages[index1]
-            let joltage2 = joltages[index2]
-            let joltage3 = joltages[index3]
-
+        for index in reaches.indices.dropFirst(3) {
+            let joltage = joltages[index]
             var reach = 0
-            if canReach(joltage0, joltage3) { reach += reaches[index0] }
-            if canReach(joltage1, joltage3) { reach += reaches[index1] }
-            if canReach(joltage2, joltage3) { reach += reaches[index2] }
-            reaches[index3] = reach
+            if canReach(joltages[index - 1], joltage) { reach += reaches[index - 1] }
+            if canReach(joltages[index - 2], joltage) { reach += reaches[index - 2] }
+            if canReach(joltages[index - 3], joltage) { reach += reaches[index - 3] }
+            reaches[index] = reach
         }
         return reaches.last!
     }
