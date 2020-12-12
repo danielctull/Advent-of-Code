f41b6202218bf09c456c10bb7d6fdf894d217ad1
Daniel Tull <dt@danieltull.co.uk>
2020-12-12 07:00:35 +0000

    Bring the perform instruction function out of the ship

--- Year2020/Sources/Day12.swift
+++ Year2020/Sources/Day12.swift
@@ -7,10 +7,22 @@ public enum Day12 {
     public static func part1(_ input: Input) throws -> Int {
 
         let regex = try RegularExpression(pattern: "^([NSEWLRF])([0-9]+)$")
-        let ship = try input.lines
+        let instructions = try input.lines
             .map(regex.match)
             .map(Instruction.init)
-            .reduce(into: Ship()) { ship, instruction in ship.perform(instruction) }
+
+        var ship = Ship()
+        for instruction in instructions {
+            switch instruction.kind {
+            case .N: ship.move(.up, instruction.amount)
+            case .E: ship.move(.right, instruction.amount)
+            case .S: ship.move(.down, instruction.amount)
+            case .W: ship.move(.left, instruction.amount)
+            case .F: ship.move(ship.facing, instruction.amount)
+            case .L: ship.rotate(.left, instruction.amount/90)
+            case .R: ship.rotate(.right, instruction.amount/90)
+            }
+        }
 
         return Position.origin.manhattenDistance(to: ship.position)
     }
@@ -40,44 +52,6 @@ public enum Day12 {
     }
 }
 
-extension Day12.Ship {
-
-    mutating func move(_ vector: Vector<Int>, _ amount: Int) {
-        (1...amount).forEach { _ in position = position.move(vector) }
-    }
-
-    mutating func move(_ direction: Direction, _ amount: Int) {
-        position = position.move(Move(direction: direction, amount: amount))
-    }
-
-    mutating func rotate(_ turn: Turn, _ amount: Int) {
-        (1...amount).forEach { _ in facing = facing.rotate(turn) }
-    }
-
-    mutating func perform(_ instruction: Day12.Instruction) {
-        switch instruction.kind {
-        case .N: move(.up, instruction.amount)
-        case .E: move(.right, instruction.amount)
-        case .S: move(.down, instruction.amount)
-        case .W: move(.left, instruction.amount)
-        case .F: move(facing, instruction.amount)
-        case .L: rotate(.left, instruction.amount/90)
-        case .R: rotate(.right, instruction.amount/90)
-        }
-    }
-}
-
-extension Day12.Waypoint {
-
-    mutating func move(_ direction: Direction, _ amount: Int) {
-        position = position.move(Move(direction: direction, amount: amount))
-    }
-
-    mutating func rotate(_ turn: Turn, _ amount: Int) {
-        (1...amount).forEach { _ in position = position.rotateAboutOrigin(turn) }
-    }
-}
-
 extension Day12 {
 
     fileprivate struct Ship {
@@ -98,6 +72,36 @@ extension Day12 {
         }
     }
 }
+
+// MARK: - Actions
+
+extension Day12.Ship {
+
+    mutating func move(_ vector: Vector<Int>, _ amount: Int) {
+        (1...amount).forEach { _ in position = position.move(vector) }
+    }
+
+    mutating func move(_ direction: Direction, _ amount: Int) {
+        position = position.move(Move(direction: direction, amount: amount))
+    }
+
+    mutating func rotate(_ turn: Turn, _ amount: Int) {
+        (1...amount).forEach { _ in facing = facing.rotate(turn) }
+    }
+}
+
+extension Day12.Waypoint {
+
+    mutating func move(_ direction: Direction, _ amount: Int) {
+        position = position.move(Move(direction: direction, amount: amount))
+    }
+
+    mutating func rotate(_ turn: Turn, _ amount: Int) {
+        (1...amount).forEach { _ in position = position.rotateAboutOrigin(turn) }
+    }
+}
+
+// MARK: - Parsing
 
 extension Day12.Instruction {
 
