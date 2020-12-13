b74aaf57b957233fbebe7ca25a4ca658a7e5faff
Daniel Tull <dt@danieltull.co.uk>
2020-12-13 11:32:23 +0000

    Trying to work through the madness

--- Year2020/Sources/Day13.swift
+++ Year2020/Sources/Day13.swift
@@ -18,10 +18,8 @@ public enum Day13 {
             .compactMap(Int.init)
 
         return try buses.map { id -> (Int, Int) in
-            let previousBus = time % id
-            if previousBus == 0 { return (id, 0) }
-            let nextBus = id - previousBus
-            return (id, nextBus)
+            let next = Bus(id: id).next(after: time)
+            return (id, next)
         }
         .min(by: { $0.1 < $1.1 })
         .map(*)
@@ -29,6 +27,125 @@ public enum Day13 {
     }
 
     public static func part2(_ input: Input) throws -> Int {
-        0
+
+        let buses = try input.lines
+            .last
+            .unwrapped()
+            .components(separatedBy: ",")
+            .map(Int.init)
+
+        let timeBuses = zip(0..., buses)
+            .compactMap { time, bus -> (Int, Bus)? in
+                guard let bus = bus else { return nil }
+                return (time, Bus(id: bus))
+            }
+
+        // For the following sequence:
+        // 7, 13, x, x, 59, x, 31, 19
+        //
+        //              77     168     259
+        // id:7  p:0    11      24      37   > +13
+        // id:13 p:1  5r12   12r12   19r12   > +7 r(13-1)
+        //
+        //             350     763    1176
+        // id:7  p:0    50     109     168   > +59
+        // id:59 p:4  5r55   12r55   19r55   > +7 r(59-4)
+        //
+        //              56    273     490
+        // id:7  p:0     8     39      70   > +31
+        // id:31 p:6  1r25   8r25   15r25   > +7 r(31-6)
+
+        // Get the bus with the time expectation of zero – should always exist.
+        let first = try timeBuses.first(where: { $0.0 == 0 }).unwrapped()
+
+        print(first)
+        print(timeBuses)
+
+        let lowests = try timeBuses
+            .dropFirst()
+            .map { time, bus -> LazyMapSequence<(PartialRangeFrom<Int>), Int> in
+
+                let lowestCommon = try (1...)
+                    .lazy
+                    .first(where: { goal in
+                        let next = bus.next(after: goal * first.1.id)
+                        return next == time
+                    })
+                    .unwrapped()
+
+                return (0...).lazy.map { $0 * bus.id + lowestCommon }
+            }
+
+        for lowest in lowests {
+            print("-------")
+            var i = lowest.makeIterator()
+            print(i.next()!) * 30
+        }
+
+//        let bus = try timeBuses
+//            .reduce { previous, next in
+//
+//                let lowestCommon = try (1...)
+//                    .lazy
+//                    .first(where: { goal in
+//                        let next = bus.next(after: goal * first.1.id)
+//                        return next == time
+//                    })
+//                    .unwrapped()
+//
+//                return (1...).lazy.map { $0 * bus.id + lowestCommon }
+//            }
+
+        return 0
+//        return try lowests
+//            .reduce(lowestCommonMultiple)
+//            .unwrapped()
+
+
+
+
+
+//        let predicates = requiredTimes
+//            .map { time, bus in
+//                Predicate { bus.next(after: $0) == time }
+//            }
+//
+//        let max = try requiredTimes
+//            .max(by: { lhs, rhs in lhs.1.id < lhs.1.id })
+//            .unwrapped()
+//
+//        // value * id - time
+//        // time / id = value
+//
+//        return try (100000000000...)
+//            .lazy
+//            .map { $0 * max.1.id - max.0 } // Only search times for the largest bus
+//            .first(where: predicates.satisfiedBy)
+//            .unwrapped()
     }
+}
+
+extension Day13 {
+
+    fileprivate struct Bus {
+        let id: Int
+    }
+
+    fileprivate struct Position: RawRepresentable {
+        let rawValue: Int
+    }
+}
+
+extension Day13.Bus {
+
+    func next(after time: Int) -> Int {
+        let previousBus = time % id
+        if previousBus == 0 { return 0 }
+        return id - previousBus
+    }
+}
+
+
+extension Day13.Bus: CustomStringConvertible {
+    var description: String { "Bus(id: \(id))" }
 }
--- Year2020/Tests/Day13Tests.swift
+++ Year2020/Tests/Day13Tests.swift
@@ -15,7 +15,61 @@ final class Day13Tests: XCTestCase {
     }
 
     func testPart2Example1() throws {
-        XCTAssertEqual(try Day13.part2([]), 0)
+        XCTAssertEqual(try Day13.part2(["UNUSED","7,13,x,x,59,x,31,19"]), 1068781)
+    }
+
+    // 7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105
+
+    // 13, 26, 39, 52, 65, 78, 91, 104
+    // 14, 27, 40, 53, 66, 79, 92, 105
+
+    // 59, 118, 177, 236, 295, 354, 413, 472
+    // 63, 122,
+
+    // For the following sequence:
+    // 7, 13, x, x, 59, x, 31, 19
+    //
+    //      14   105    196    287    378    469    560
+    //  7:   2    15     28     41     54     67     80   > +13
+    // 13: 1r1   8r1   15r1   22r1   29r1   36r1   43r1   > + 7
+    //
+    //      63   476    889   1302   1715
+    //  7:   9    68    127    186    245                 > +59
+    // 59: 1r4   8r4   15r4   22r4   29r4   36r4   43r4   > + 7
+
+
+
+    // 7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105, 112, 119, 126, 133, 140, 147, 154, 161, 168, 175
+
+    // 13, 26, 39, 52, 65, 78, 91, 104, 117, 130, 143, 156, 169, 182
+    // 12, 25, 38, 51, 64, 77, 90, 103, 116, 129, 142, 155, 168, 181
+
+    // For the following sequence:
+    // 7, 13, x, x, 59, x, 31, 19
+    //
+    //       77    168
+    //  7:   11     24       > +13
+    // 13: 5r12   8r12       > + 7
+    //
+    //      63   476    889   1302   1715
+    //  7:   9    68    127    186    245                 > +59
+    // 59: 1r4   8r4   15r4   22r4   29r4   36r4   43r4   > + 7
+
+
+    func testPart2Example2() throws {
+        XCTAssertEqual(try Day13.part2(["UNUSED","67,7,59,61"]), 754018)
+    }
+
+    func testPart2Example3() throws {
+        XCTAssertEqual(try Day13.part2(["UNUSED","67,x,7,59,61"]), 779210)
+    }
+
+    func testPart2Example4() throws {
+        XCTAssertEqual(try Day13.part2(["UNUSED","67,7,x,59,61"]), 1261476)
+    }
+    
+    func testPart2Example5() throws {
+        XCTAssertEqual(try Day13.part2(["UNUSED","1789,37,47,1889"]), 1202161486)
     }
 
     func testPart2Puzzle() throws {
