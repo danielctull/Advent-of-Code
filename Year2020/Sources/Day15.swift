e27075263c427564fd93fb8b76c2dcfe05bb8cce
Daniel Tull <dt@danieltull.co.uk>
2020-12-15 06:29:47 +0000

    Refactor day 15 into a single function

--- Year2020/Sources/Day15.swift
+++ Year2020/Sources/Day15.swift
@@ -7,32 +7,14 @@ public enum Day15: Day {
     public static let title = "Rambunctious Recitation"
 
     public static func part1(_ input: Input) throws -> Int {
-
-        var positions = try zip(1..., input.integers)
-            .dropLast()
-            .group { $0.1 }
-            .mapValues { try $0.last.unwrapped().0 }
-
-        let last = try input
-            .integers
-            .last
-            .unwrapped()
-
-        return (input.integers.count..<2020).reduce(last) { previous, turn in
-
-            let next: Int
-            if let previousTurn = positions[previous] {
-                next = turn - previousTurn
-            } else {
-                next = 0
-            }
-
-            positions[previous] = turn
-            return next
-        }
+        try findNumber(2020, in: input)
     }
 
     public static func part2(_ input: Input) throws -> Int {
+        try findNumber(30000000, in: input)
+    }
+
+    private static func findNumber(_ goal: Int, in input: Input) throws -> Int {
 
         var positions = try zip(1..., input.integers)
             .dropLast()
@@ -44,7 +26,7 @@ public enum Day15: Day {
             .last
             .unwrapped()
 
-        return (input.integers.count..<30000000).reduce(last) { previous, turn in
+        return (input.integers.count..<goal).reduce(last) { previous, turn in
 
             let next: Int
             if let previousTurn = positions[previous] {
