0d4bb65d63b349a2192829c5f50effda040b7894
Daniel Tull <dt@danieltull.co.uk>
2020-12-16 09:17:30 +0000

    Add 2020, day 16, part 2

--- Year2020/Sources/Day16.swift
+++ Year2020/Sources/Day16.swift
@@ -12,6 +12,7 @@ public enum Day16: Day {
             .split(whereSeparator: \.isEmpty)
 
         let rules = try Array(rules: split[0])
+            .map(\.valid)
             .joined(operator: ||)
 
         let values = try split[2]
@@ -26,21 +27,107 @@ public enum Day16: Day {
     }
 
     public static func part2(_ input: Input) throws -> Int {
-        0
+
+        let split = input.lines
+            .split(whereSeparator: \.isEmpty)
+
+        let rules = try Array(rules: split[0])
+        let yourTicket = try Array(tickets: split[1]).first.unwrapped()
+        let nearbyTickets = try Array(tickets: split[2])
+
+        let anyRule = rules
+            .map(\.valid)
+            .joined(operator: ||)
+        let validTickets = nearbyTickets
+            .filter { $0.fields.allSatisfy(anyRule) }
+
+        let potentialRules = Array(repeating: rules, count: yourTicket.fields.count)
+
+        return try validTickets
+
+            // Remove any which aren't valid for the fields in the tickets.
+            .reduce(potentialRules) { potentialRules, ticket in
+                zip(potentialRules, ticket.fields)
+                    .map { potential, field in
+                        potential.filter { $0.valid(field) }
+                    }
+            }
+
+            // Order them by number of potential rules so the ones with the
+            // fewest will be looked at first.
+            .enumerated()
+            .sorted(by: { $0.element.count < $1.element.count })
+
+            // Move along the [[Rule]] and pick the first where the rules aren't
+            // already used for a previous field.
+            .reduce(into: Array<(Int, Rule)>()) { rules, potential in
+
+                let rule = try potential
+                    .element
+                    .first(where: { rule in !rules.contains { $0.1 == rule }})
+                    .unwrapped()
+
+                rules.append((potential.offset, rule))
+            }
+
+            // Find the rules that have the "depature" prefix and multiply the
+            // fields in your ticket.
+            .filter { $0.1.name.hasPrefix("departure") }
+            .map { yourTicket.fields[$0.0] }
+            .product()
     }
 }
 
-extension Array where Element == Predicate<Int> {
+extension Day16 {
+
+    fileprivate struct Ticket {
+        let fields: [Int]
+    }
+
+    fileprivate struct Rule {
+        let name: String
+        let valid: Predicate<Int>
+    }
+}
+
+extension Day16.Rule: Equatable {
+    static func == (lhs: Day16.Rule, rhs: Day16.Rule) -> Bool {
+        lhs.name == rhs.name
+    }
+}
+
+extension Day16.Rule: CustomStringConvertible {
+    var description: String { "Rule: \(name)" }
+}
+
+extension Array where Element == Day16.Rule {
 
     fileprivate init<S>(rules: S) throws where S: Sequence, S.Element == String {
-        let regex = try RegularExpression(pattern: "^[a-z ]+: ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)")
+        let regex = try RegularExpression(pattern: "^([a-z ]+): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)")
         self = try rules.map { string in
             let match = try regex.match(string)
-            let lower1 = try match.integer(at: 0)
-            let upper1 = try match.integer(at: 1)
-            let lower2 = try match.integer(at: 2)
-            let upper2 = try match.integer(at: 3)
-            return .isWithin(lower1...upper1) || .isWithin(lower2...upper2)
+            let lower1 = try match.integer(at: 1)
+            let upper1 = try match.integer(at: 2)
+            let lower2 = try match.integer(at: 3)
+            let upper2 = try match.integer(at: 4)
+            return Day16.Rule(
+                name: try match.string(at: 0),
+                valid: .isWithin(lower1...upper1) || .isWithin(lower2...upper2))
         }
+    }
+}
+
+extension Array where Element == Day16.Ticket {
+
+    fileprivate init<S>(tickets: S) throws where S: Sequence, S.Element == String {
+
+        self = try tickets
+            .dropFirst()
+            .map { string in
+
+                Day16.Ticket(
+                    fields: try string.split(separator: ",")
+                        .map(Int.init))
+            }
     }
 }
--- Year2020/Tests/Day16Tests.swift
+++ Year2020/Tests/Day16Tests.swift
@@ -27,12 +27,25 @@ final class Day16Tests: XCTestCase {
         XCTAssertEqual(try Day16.part1(input), 26053)
     }
 
+
     func testPart2Examples() throws {
-        XCTAssertEqual(try Day16.part2([]), 0)
+        XCTAssertEqual(try Day16.part2([
+            "class: 0-1 or 4-19",
+            "departure row: 0-5 or 8-19",
+            "departure seat: 0-13 or 16-19",
+            "",
+            "your ticket:",
+            "11,12,13",
+            "",
+            "nearby tickets:",
+            "3,9,18",
+            "15,1,5",
+            "5,14,9"
+        ]), 143)
     }
 
     func testPart2Puzzle() throws {
         let input = try Bundle.module.input(named: "Day16")
-        XCTAssertEqual(try Day16.part2(input), 0)
+        XCTAssertEqual(try Day16.part2(input), 1515506256421)
     }
 }
