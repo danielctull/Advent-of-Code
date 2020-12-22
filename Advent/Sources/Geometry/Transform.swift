9eb879c9a99b03af6bd8397332a1830306207fcd
Daniel Tull <dt@danieltull.co.uk>
2020-12-22 07:30:10 +0000

    Define transform with a generic for the dimensionality

--- Advent/Sources/Geometry/Transform.swift
+++ Advent/Sources/Geometry/Transform.swift
@@ -1,20 +1,41 @@
 
-public struct Transform<Space: Dimension, Scalar: Numeric> where Space.Scalar == Scalar {
-
-    let storage: Dimension2<Space>
+public struct Transform<Storage, Space, Scalar>
+    where
+    Storage: Dimension,
+    Space: Dimension,
+    Space.Scalar == Scalar,
+    Storage.Scalar == Space,
+    Storage.Parameter == Space.Parameter
+{
+    let storage: Storage
 }
 
-public typealias Transform2D<Scalar: Numeric> = Transform<Dimension2<Scalar>, Scalar>
+public typealias Transform2D<Scalar: Numeric> = Transform<Dimension2<Dimension2<Scalar>>, Dimension2<Scalar>, Scalar>
 
-extension Transform2D where Space == Dimension2<Scalar> {
+extension Transform2D where Scalar: Numeric, Storage == Dimension2<Dimension2<Scalar>> {
 
     public init(_ xx: Scalar, _ xy: Scalar, _ yx: Scalar, _ yy: Scalar) {
-        storage = Dimension2(
-            x: Dimension2(x: xx, y: xy),
-            y: Dimension2(x: yx, y: yy))
+        storage = Storage {
+            switch $0 {
+            case .x:
+                return Space {
+                    switch $0 {
+                    case .x: return xx
+                    case .y: return xy
+                    }
+                }
+            case .y:
+                return Space { inner in
+                    switch inner {
+                    case .x: return yx
+                    case .y: return yy
+                    }
+                }
+            }
+        }
     }
 
-    public static var identity: Self { Transform2D(1, 0, 0, 1) }
-    public static var rotate90: Self { Transform2D(0, 1, -1, 0) }
-    public static var rotate270: Self { Transform2D(0, -1, 1, 0) }
+    public static var identity: Self { Self(1, 0, 0, 1) }
+    public static var rotate90: Self { Self(0, 1, -1, 0) }
+    public static var rotate270: Self { Self(0, -1, 1, 0) }
 }
