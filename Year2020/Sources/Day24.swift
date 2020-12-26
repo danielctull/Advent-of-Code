99a198da3f0e767921120862f1652437b15940cb
Daniel Tull <dt@danieltull.co.uk>
2020-12-26 08:45:38 +0000

    Add 2020, day 24, part 2

--- Year2020/Sources/Day24.swift
+++ Year2020/Sources/Day24.swift
@@ -22,7 +22,35 @@ public enum Day24: Day {
     }
 
     public static func part2(_ input: Input) throws -> Int {
-        0
+
+        let regex = try RegularExpression(pattern: "(ne|nw|se|sw|e|w)")
+        let dictionary = try input.lines.map {
+
+            try regex
+                .matches(in: $0)
+                .map { try $0.string(at: 0) }
+                .map(Vector<HexagonalDimension, Int>.init)
+                .reduce(Position.origin) { $0 + $1 }
+        }
+        .group(by: { $0 })
+        .filter { !$0.value.count.isMultiple(of: 2) }
+
+        return (1...100).reduce(Set(dictionary.keys)) { tiles, day in
+
+            let existingBlackTiles = tiles.filter {
+                let neighbours = $0.neighbours.intersection(tiles).count
+                return neighbours == 1 || neighbours == 2
+            }
+
+            let newBlackTiles = Set(tiles.flatMap(\.neighbours)).filter {
+                guard !tiles.contains($0) else { return false }
+                let neighbours = $0.neighbours.intersection(tiles).count
+                return neighbours == 2
+            }
+
+            return existingBlackTiles.union(newBlackTiles)
+        }
+        .count
     }
 }
 
@@ -56,6 +84,33 @@ extension Vector where Space == HexagonalDimension<Scalar> {
             case .s: return 0 - q - r
             }
         }
+    }
+}
+
+extension Position where Space == HexagonalDimension<Scalar> {
+
+    public init(q: Scalar, r: Scalar) {
+        self.init { parameter in
+            switch parameter {
+            case .q: return q
+            case .r: return r
+            case .s: return 0 - q - r
+            }
+        }
+    }
+}
+
+extension Position where Space == HexagonalDimension<Scalar>, Scalar: Hashable {
+
+    public var neighbours: Set<Self> {
+        Set([
+            Self(q: self.q + 1, r: self.r + 0), // s - 1
+            Self(q: self.q + 1, r: self.r - 1), // s + 0
+            Self(q: self.q + 0, r: self.r + 1), // s - 1
+            Self(q: self.q + 0, r: self.r - 1), // s + 1
+            Self(q: self.q - 1, r: self.r + 0), // s + 1
+            Self(q: self.q - 1, r: self.r + 1), // s + 0
+        ])
     }
 }
 
--- Year2020/Tests/Day24Tests.swift
+++ Year2020/Tests/Day24Tests.swift
@@ -36,11 +36,31 @@ final class Day24Tests: XCTestCase {
     }
 
     func testPart2Example() throws {
-        XCTAssertEqual(try Day24.part2(""), 0)
-    }
+        XCTAssertEqual(try Day24.part2([
+            "sesenwnenenewseeswwswswwnenewsewsw",
+            "neeenesenwnwwswnenewnwwsewnenwseswesw",
+            "seswneswswsenwwnwse",
+            "nwnwneseeswswnenewneswwnewseswneseene",
+            "swweswneswnenwsewnwneneseenw",
+            "eesenwseswswnenwswnwnwsewwnwsene",
+            "sewnenenenesenwsewnenwwwse",
+            "wenwwweseeeweswwwnwwe",
+            "wsweesenenewnwwnwsenewsenwwsesesenwne",
+            "neeswseenwwswnwswswnw",
+            "nenwswwsewswnenenewsenwsenwnesesenew",
+            "enewnwewneswsewnwswenweswnenwsenwsw",
+            "sweneswneswneneenwnewenewwneswswnese",
+            "swwesenesewenwneswnwwneseswwne",
+            "enesenwswwswneneswsenwnewswseenwsese",
+            "wnwnesenesenenwwnenwsewesewsesesew",
+            "nenewswnwewswnenesenwnesewesw",
+            "eneswnwswnwsenenwnwnwwseeswneewsenese",
+            "neswnwewnwnwseenwseesewsenwsweewe",
+            "wseweeenwnesenwwwswnew"
+        ]), 2208)    }
 
     func testPart2Puzzle() throws {
         let input = try Bundle.module.input(named: "Day24")
-        XCTAssertEqual(try Day24.part2(input), 0)
+        XCTAssertEqual(try Day24.part2(input), 3869)
     }
 }
