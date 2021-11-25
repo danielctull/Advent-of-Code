544740c9337173e3e226414486d407b6b9062297
Daniel Tull <dt@danieltull.co.uk>
2021-11-25 21:52:09 +0000

    Refactor the game into a Game struct

--- Year2020/Sources/Day22.swift
+++ Year2020/Sources/Day22.swift
@@ -7,42 +7,53 @@ public enum Day22: Day {
     public static let title = "Crab Combat"
 
     public static func part1(_ input: Input) throws -> Int {
-        var (lhs, rhs) = try parse(input)
-        combat(&lhs, &rhs)
-        return max(lhs.score, rhs.score)
+        var game = try Game(input)
+        game.combat()
+        return game.score
     }
 
     public static func part2(_ input: Input) throws -> Int {
         0
     }
-
-    private static func parse(_ input: Input) throws -> (Deck, Deck) {
-        let decks = input.lines
-            .split(separator: "")
-            .map { Deck(cards: $0.compactMap(Int.init)) }
-        return try (decks.first.unwrapped(), decks.last.unwrapped())
-    }
-
-    private static func combat(_ lhs: inout Deck, _ rhs: inout Deck) {
-        while !lhs.cards.isEmpty && !rhs.cards.isEmpty {
-            let left = lhs.cards.removeFirst()
-            let right = rhs.cards.removeFirst()
-            if left > right {
-                lhs.cards.append(left)
-                lhs.cards.append(right)
-            } else {
-                rhs.cards.append(right)
-                rhs.cards.append(left)
-            }
-        }
-    }
 }
 
 extension Day22 {
 
+    fileprivate struct Game {
+        var player1: Deck
+        var player2: Deck
+    }
+
     fileprivate struct Deck {
         var cards: [Int]
     }
+}
+
+extension Day22.Game {
+
+    init(_ input: Input) throws {
+        let decks = input.lines
+            .split(separator: "")
+            .map { Day22.Deck(cards: $0.compactMap(Int.init)) }
+        player1 = try decks.first.unwrapped()
+        player2 = try decks.last.unwrapped()
+    }
+
+    mutating func combat() {
+        while !player1.cards.isEmpty && !player2.cards.isEmpty {
+            let card1 = player1.cards.removeFirst()
+            let card2 = player2.cards.removeFirst()
+            if card1 > card2 {
+                player1.cards.append(card1)
+                player1.cards.append(card2)
+            } else {
+                player2.cards.append(card2)
+                player2.cards.append(card1)
+            }
+        }
+    }
+
+    var score: Int { max(player1.score, player2.score) }
 }
 
 extension Day22.Deck {
