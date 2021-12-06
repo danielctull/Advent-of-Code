566ab17e50543981731c0c849a06b16601a8f50a
Daniel Tull <dt@danieltull.co.uk>
2021-12-06 18:09:13 +0000

    Improve speed of 2021, day 6, part 1
    This is en route to a better solution for part 2

--- Year2021/Sources/Day06.swift
+++ Year2021/Sources/Day06.swift
@@ -8,6 +8,22 @@ public enum Day06: Day {
     public static let title = "Lanternfish"
 
     public static func part1(_ input: Input) throws -> Int {
+        try run(input, amount: 80)
+    }
+
+    public static func part2(_ input: Input) throws -> Int {
+        try run(input, amount: 256)
+    }
+
+    private static func run(_ input: Input, amount: Int) throws -> Int {
+
+        let eightDays = (0...9)
+            .map(Lanternfish.init)
+            .map { lanternfish in
+                (1...8).reduce([lanternfish]) { fish, _ in fish.flatMap(\.ticktock) }
+            }
+
+        let amount = amount/8
 
         let lanternfish = try input.lines
             .first.unwrapped
@@ -15,19 +31,17 @@ public enum Day06: Day {
             .map(Int.init)
             .map(Lanternfish.init)
 
-        return (1...80)
-            .reduce(lanternfish) { fish, _ in fish.flatMap(\.ticktock) }
+        return (1...amount)
+            .reduce(lanternfish) { fish, _ in
+                fish.flatMap { eightDays[$0.timer] }
+            }
             .count
-    }
-
-    public static func part2(_ input: Input) throws -> Int {
-        0
     }
 }
 
 extension Day06 {
 
-    struct Lanternfish {
+    struct Lanternfish: Hashable {
         let timer: Int
     }
 }
