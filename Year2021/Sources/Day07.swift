3b8b18421fb183b773e5c5600f14ec01960db017
Daniel Tull <dt@danieltull.co.uk>
2021-12-07 09:47:12 +0000

    Generalise parts 1 and 2

--- Year2021/Sources/Day07.swift
+++ Year2021/Sources/Day07.swift
@@ -8,27 +8,17 @@ public enum Day07: Day {
     public static let title = "The Treachery of Whales"
 
     public static func part1(_ input: Input) throws -> Int {
-
-        let positions = try input.lines
-            .first.unwrapped
-            .split(separator: ",")
-            .map(Int.init)
-
-        let min = try positions.min().unwrapped
-        let max = try positions.max().unwrapped
-        let costs = positions.countByElement
-
-        return try (min...max).map { proposed -> Int in
-            costs.map { position, cost in
-                abs(proposed - position) * cost
-            }
-            .sum
-        }
-        .min()
-        .unwrapped
+        try run(input, cost: { abs($0 - $1) })
     }
 
     public static func part2(_ input: Input) throws -> Int {
+        try run(input, cost: { abs($0 - $1).triangle })
+    }
+
+    private static func run(
+        _ input: Input,
+        cost: (Int, Int) -> Int
+    ) throws -> Int {
 
         let positions = try input.lines
             .first.unwrapped
@@ -37,11 +27,11 @@ public enum Day07: Day {
 
         let min = try positions.min().unwrapped
         let max = try positions.max().unwrapped
-        let costs = positions.countByElement
+        let amounts = positions.countByElement
 
         return try (min...max).map { proposed -> Int in
-            costs.map { position, cost in
-                abs(proposed - position).triangle * cost
+            amounts.map { position, amount in
+                cost(proposed, position) * amount
             }
             .sum
         }
