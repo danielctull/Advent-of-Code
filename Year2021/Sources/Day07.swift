7b4895ab2b0696ab013a13dff36bd5e079e6ac83
Daniel Tull <dt@danieltull.co.uk>
2021-12-07 09:21:09 +0000

    Add 2021, day 7, part 1

---
+++ Year2021/Sources/Day07.swift
@@ -0,0 +1,34 @@
+
+import Advent
+import Algorithms
+import Foundation
+
+public enum Day07: Day {
+
+    public static let title = "The Treachery of Whales"
+
+    public static func part1(_ input: Input) throws -> Int {
+
+        let positions = try input.lines
+            .first.unwrapped
+            .split(separator: ",")
+            .map(Int.init)
+
+        let min = try positions.min().unwrapped
+        let max = try positions.max().unwrapped
+        let costs = positions.countByElement
+
+        return try (min...max).map { proposed -> Int in
+            costs.map { position, cost in
+                abs(proposed - position) * cost
+            }
+            .sum
+        }
+        .min()
+        .unwrapped
+    }
+
+    public static func part2(_ input: Input) throws -> Int {
+        0
+    }
+}
---
+++ Year2021/Tests/Day07Tests.swift
@@ -0,0 +1,25 @@
+
+import Advent
+import Year2021
+import XCTest
+
+final class Day07Tests: XCTestCase {
+
+    func testPart1Examples() throws {
+        XCTAssertEqual(try Day07.part1(["16,1,2,0,4,2,7,1,2,14"]), 37)
+    }
+
+    func testPart1Puzzle() throws {
+        let input = try Bundle.module.input(named: "Day07")
+        XCTAssertEqual(try Day07.part1(input), 344535)
+    }
+
+    func testPart2Examples() throws {
+        XCTAssertEqual(try Day07.part2([]), 0)
+    }
+
+    func testPart2Puzzle() throws {
+        let input = try Bundle.module.input(named: "Day07")
+        XCTAssertEqual(try Day07.part2(input), 0)
+    }
+}
