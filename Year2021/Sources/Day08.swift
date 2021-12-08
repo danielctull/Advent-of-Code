ece12ce124f7da500ed0f26f1c28a6d339a0b835
Daniel Tull <dt@danieltull.co.uk>
2021-12-08 10:46:44 +0000

    Add 2021, day 8, part 2

--- Year2021/Sources/Day08.swift
+++ Year2021/Sources/Day08.swift
@@ -19,6 +19,66 @@ public enum Day08: Day {
     }
 
     public static func part2(_ input: Input) throws -> Int {
-        0
+        try input.lines
+            .map { line in
+                let segments = line.components(separatedBy: "|")
+                let mapping = try Mapping(patterns: segments.first.unwrapped)
+                return try mapping.value(for: segments.last.unwrapped)
+            }
+            .sum
+    }
+}
+
+extension Day08 {
+
+    struct Mapping {
+        let dictionary: [Set<Character>: Int]
+    }
+}
+
+//  #1:          c        f       * Initial
+//  #7:    a     c        f       * Initial
+//  #4:       b  c  d     f       * Initial
+//  #2:    a     c  d  e     g    * 5 characters not #5 or #3
+//  #5:    a  b     d     f  g    * 5 characters superset of (#4 subtracting #7)
+//  #3:    a     c  d     f  g    * 5 characters superset of #1
+//  #9:    a  b  c  d     f  g    * 6 characters superset of #4
+//  #0:    a  b  c     e  f  g    * 6 characters not #9 or #6
+//  #6:    a  b     d  e  f  g    * 6 characters not superset of #1
+//  #8:    a  b  c  d  e  f  g    * Initial
+
+extension Day08.Mapping {
+
+    init(patterns: String) throws {
+
+        let patterns = patterns
+            .components(separatedBy: .whitespaces)
+            .filter { !$0.isEmpty }
+            .map(Set.init)
+
+        let one   = try patterns.first(where: { $0.count == 2 }).unwrapped
+        let seven = try patterns.first(where: { $0.count == 3 }).unwrapped
+        let four  = try patterns.first(where: { $0.count == 4 }).unwrapped
+        let eight = try patterns.first(where: { $0.count == 7 }).unwrapped
+        let three = try patterns.first(where: { $0.count == 5 && $0.isSuperset(of: one) }).unwrapped
+        let five  = try patterns.first(where: { $0.count == 5 && $0.isSuperset(of: four.subtracting(seven)) }).unwrapped
+        let two   = try patterns.first(where: { $0.count == 5 && ![three, five].contains($0) }).unwrapped
+        let nine  = try patterns.first(where: { $0.count == 6 && $0.isSuperset(of: four) }).unwrapped
+        let six   = try patterns.first(where: { $0.count == 6 && !$0.isSuperset(of: one) }).unwrapped
+        let zero  = try patterns.first(where: { $0.count == 6 && ![nine, six].contains($0) }).unwrapped
+
+        self.dictionary = [zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9]
+    }
+
+    func value(for output: String) throws -> Int {
+        try output
+            .components(separatedBy: .whitespaces)
+            .filter { !$0.isEmpty }
+            .map(Set.init)
+            .map { try dictionary[$0].unwrapped }
+            .reversed()
+            .enumerated()
+            .map { index, value in value * 10.power(index) }
+            .sum
     }
 }
--- Year2021/Tests/Day08Tests.swift
+++ Year2021/Tests/Day08Tests.swift
@@ -25,12 +25,29 @@ final class Day08Tests: XCTestCase {
         XCTAssertEqual(try Day08.part1(input), 534)
     }
 
-    func testPart2Examples() throws {
-        XCTAssertEqual(try Day08.part2([]), 0)
+    func testPart2Example1() throws {
+        XCTAssertEqual(try Day08.part2([
+            "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf",
+        ]), 5353)
+    }
+
+    func testPart2Example2() throws {
+        XCTAssertEqual(try Day08.part2([
+            "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+            "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+            "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+            "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+            "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+            "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+            "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+            "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+            "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+            "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
+        ]), 61229)
     }
 
     func testPart2Puzzle() throws {
         let input = try Bundle.module.input(named: "Day08")
-        XCTAssertEqual(try Day08.part2(input), 0)
+        XCTAssertEqual(try Day08.part2(input), 1070188)
     }
 }
