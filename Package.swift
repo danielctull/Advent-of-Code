f8a057ebcc2c675d146bc75fa91e10258c1ff02a
Daniel Tull <dt@danieltull.co.uk>
2022-11-22 16:48:06 +0000

    Update ArgumentParser

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms", from: "0.0.4"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-crypto", from: "2.2.0"),
         .package(url: "https://github.com/danielctull/FileBuilder", branch: "main"),
     ],
