c37293a7b4aa8513ab3e6bc509c5362bb005481a
Daniel Tull <dt@danieltull.co.uk>
2022-12-02 08:12:52 +0000

    Refactor part 1

--- Year2022/Sources/Day02.swift
+++ Year2022/Sources/Day02.swift
@@ -9,8 +9,8 @@ public enum Day02: Day {
 
     public static func part1(_ input: Input) throws -> Int {
         try input.lines
-            .map(Round.init)
-            .map(\.score)
+            .map { try (theirs: Shape($0.first), mine: Shape($0.last)) }
+            .map { $0.theirs.result(with: $0.mine).rawValue + $0.mine.rawValue }
             .sum
     }
 
@@ -21,75 +21,35 @@ public enum Day02: Day {
 
 extension Day02 {
 
-    fileprivate struct Round {
-        let theirs: Shape
-        let mine: Shape
+    fileprivate enum Shape: Int {
+        case rock = 1
+        case paper = 2
+        case scissors = 3
     }
 
-    fileprivate enum Shape {
-        case rock
-        case paper
-        case scissors
-    }
-}
-
-extension Day02.Round: RawRepresentable {
-
-    init?(rawValue: String) {
-        guard let first = rawValue.first, let last = rawValue.last else { return nil }
-        switch first {
-        case "A": theirs = .rock
-        case "B": theirs = .paper
-        case "C": theirs = .scissors
-        default: return nil
-        }
-        switch last {
-        case "X": mine = .rock
-        case "Y": mine = .paper
-        case "Z": mine = .scissors
-        default: return nil
-        }
-    }
-
-    var rawValue: String {
-        let first: Character
-        switch theirs {
-        case .rock: first = "A"
-        case .paper: first = "B"
-        case .scissors: first = "C"
-        }
-        let last: Character
-        switch mine {
-        case .rock: last = "X"
-        case .paper: last = "Y"
-        case .scissors: last = "Z"
-        }
-        return String([first, " ", last])
-    }
-}
-
-extension Day02.Round {
-
-    var result: Int {
-        switch (theirs, mine) {
-        case (.rock, .paper), (.paper, .scissors), (.scissors, .rock): return 6
-        case (.rock, .rock), (.paper, .paper), (.scissors, .scissors): return 3
-        case (.rock, .scissors), (.paper, .rock), (.scissors, .paper): return 0
-        }
-    }
-
-    var score: Int {
-        result + mine.score
+    fileprivate enum Result: Int {
+        case win = 6
+        case draw = 3
+        case lose = 0
     }
 }
 
 extension Day02.Shape {
 
-    var score: Int {
-        switch self {
-        case .rock: return 1
-        case .paper: return 2
-        case .scissors: return 3
+    init(_ character: Character?) throws {
+        switch character {
+        case "A", "X": self = .rock
+        case "B", "Y": self = .paper
+        case "C", "Z": self = .scissors
+        default: throw UnexpectedRawValue(rawValue: character)
+        }
+    }
+
+    func result(with mine: Self) -> Day02.Result {
+        switch (self, mine) {
+        case (.rock, .paper), (.paper, .scissors), (.scissors, .rock): return .win
+        case (.rock, .rock), (.paper, .paper), (.scissors, .scissors): return .draw
+        case (.rock, .scissors), (.paper, .rock), (.scissors, .paper): return .lose
         }
     }
 }
