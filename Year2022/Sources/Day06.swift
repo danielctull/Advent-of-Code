e2d741786b69c01f34bf1125dc58daee2f1a0ed6
Daniel Tull <dt@danieltull.co.uk>
2022-12-06 08:44:04 +0000

    Use the fact that characters is an array with Int based indices

--- Year2022/Sources/Day06.swift
+++ Year2022/Sources/Day06.swift
@@ -7,16 +7,18 @@ public enum Day06: Day {
     public static let title = "Tuning Trouble"
 
     public static func part1(_ input: Input) throws -> Int {
-        let line = input.characters
-        let windows = line.windows(ofCount: 4)
-        let index = try windows.firstIndex { Set($0).count == 4 }.unwrapped
-        return windows.distance(from: windows.startIndex, to: index) + 4
+        try input.characters
+            .windows(ofCount: 4)
+            .first(where: { Set($0).count == 4 })
+            .unwrapped
+            .endIndex
     }
 
     public static func part2(_ input: Input) throws -> Int {
-        let line = input.characters
-        let windows = line.windows(ofCount: 14)
-        let index = try windows.firstIndex { Set($0).count == 14 }.unwrapped
-        return windows.distance(from: windows.startIndex, to: index) + 14
+        try input.characters
+            .windows(ofCount: 14)
+            .first(where: { Set($0).count == 14 })
+            .unwrapped
+            .endIndex
     }
 }
